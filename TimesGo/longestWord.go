7aa0f12a113ab53894ed74cb7cd74991a2ffbb14
Allison Bolen <allybolen629@gmail.com>
2019-01-27 14:55:07 -0500

    longest word

---
+++ TimesGo/longestWord.go
@@ -0,0 +1,20 @@
+package main
+// Write a function that extracts the longest word from a string of space-separated words.
+import (
+	"fmt"
+  "strings"
+  "sort"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+  word := longest("this is a test for NYT")
+  fmt.Println("This is the longest word in the string: '" + word +"'")
+}
+
+func longest(a string) string {
+  words := strings.Fields(a) // split string by spaces
+  sort.Strings(words) // sort string list by increasing order
+  longest := words[len(words)-1]
+  return longest
+}
