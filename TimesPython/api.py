cdcb60549ca5483d45bdc7770552a81de5fbeadb
Allison Bolen <allybolen629@gmail.com>
2019-01-27 15:50:20 -0500

    initial

---
+++ TimesPython/api.py
@@ -0,0 +1,3 @@
+# Write a function for each RESTful operation for a resource named “document.”
+# Make sure each CRUD operation are represented by a single function.
+# The hostname can be arbitrary.
